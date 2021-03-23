fbf58e5eb27253cb8830805fe43e4f038cc9b660
Aashish Bake <aashishbake09@gmail.com>
2021-03-23 09:47:34 +0545

    check.py

---
+++ 2.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the first value:"))
+num2 = int(input("enter the second value:"))
+num3 = int(input("enter the third value:"))
+sum = num1 + num2 + num3
+print(f" The sum of the given three number is{sum}")
+
